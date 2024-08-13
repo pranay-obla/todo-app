94fd42733ab87860dc4640c24cc1691b16c7bc2a
Pranay Obla <apranayobla@gmail.com>
2024-08-14 01:17:24 +0530

    allow host

--- djangotodoapp/settings.py
+++ djangotodoapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^+7)g-gu1r@h1_wokdu$_qw=2t^9svhsmx3x!i-de#(hem*&d+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = [".vercel.app", "127.0.0.1"]
 
 
 # Application definition
